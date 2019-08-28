a9fa695fb18cd287c7c3877da9c397946cf96655
takumiando <takumiando@m.u-tokyo.ac.jp>
2019-08-28 10:02:45 +0900

    updated readme and fixed vid2img.py

--- README.md
+++ README.md
@@ -1,14 +1,26 @@
 # vid2img
 Extract frames from a video. Keep it simple stupid.
 
+# Extentions
+- Worked
+	+ mp4, 3gp, avi, flv, m4v, mov, mpeg, vob, mpg, mts, wmv
+- NOT Worked
+	+ webm, mkvm
+
 # Requirements
-```bash: Requirements
+```
 pip install -r requirements.txt
 ```
 
 # Usage
-```bash: Usage
+- usage
+```
 python vid2img.py [-h] [-o OUTDIR] [-i INTERVAL] [-e EXTENTION] [-p] path
+```
+
+- default
+```
+python vid2img.py path -o extracted -i 60 -e png
 ```
 
 # Be careful
--- vid2img.py
+++ vid2img.py
@@ -7,10 +7,11 @@ from joblib import Parallel, delayed
 
 class Vid2Img:
 
-    def __init__(self, path, outdir, interval):
+    def __init__(self, path, outdir, interval, ext):
         self.path = path
         self.outdir = outdir
         self.interval = interval
+        self.ext = ext
         self.cores = os.cpu_count()
         self.make_outdir()
         self.read_vid()
@@ -47,7 +48,7 @@ class Vid2Img:
             frame_cnt = int(vid.get(cv2.CAP_PROP_POS_FRAMES))
             ret, frame = vid.read()
             if frame_cnt % self.interval == 0 and ret:
-                cv2.imwrite(f"{Path(self.outdir)/str(frame_cnt)}.png", frame)
+                cv2.imwrite(f"{Path(self.outdir)/str(frame_cnt)}.{self.ext}", frame)
 
 
 if __name__ == '__main__':
@@ -58,11 +59,11 @@ if __name__ == '__main__':
                         help="Directory to save the frames.")
     parser.add_argument("-i", "--interval", type=int, default=60,
                         help="Interval between the frames to save.")
-    parser.add_argument("-e", "--extention", type=str, default="png",
+    parser.add_argument("-e", "--extension", type=str, default="png",
                         help="Extension to save frames as.")
     parser.add_argument("-p", "--parallel", default=False, action='store_true',
                         help="Run the extraction in parallel.")
     args = parser.parse_args()
 
-    vi = Vid2Img(args.path, args.outdir, args.interval)
+    vi = Vid2Img(args.path, args.outdir, args.interval, args.extension)
     vi.save_frames(args.parallel)
